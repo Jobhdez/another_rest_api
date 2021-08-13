a28d2f183a001053dfc3765aab6a2feab6efe4b9
Job <hj93@protonmail.com>
2021-08-13 07:23:22 -0700

    added new apps to settings.

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'papers',
+    'papers_api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
